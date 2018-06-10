893e0a3b8734bca9a39f107f6235363fdb140754
Дмитрий Быков <bd2051@mail.ru>
2018-06-10 21:47:56 +0300

    добавлена фукция прорисовки текста, добавлены отдельные переменные для повторяющихся вычислений

--- js/stat.js
+++ js/stat.js
@@ -21,6 +21,7 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
   ctx.shadowOffsetY = 10;
   ctx.shadowOffsetX = 10;
+
   ctx.fillStyle = 'white';
   ctx.beginPath();
   ctx.moveTo(CLOUD_X + CLOUD_CUT_X, CLOUD_Y);
@@ -37,10 +38,15 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.shadowColor = 'transparent';
   ctx.shadowOffsetY = 0;
   ctx.shadowOffsetX = 0;
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_MARGIN_RIGHT, CLOUD_Y + TEXT_MARGIN_TOP);
-  ctx.fillText('Список результатов:', CLOUD_X + TEXT_MARGIN_RIGHT, CLOUD_Y + TEXT_MARGIN_TOP + TEXT_LINE_HEIGHT);
+
+  var drawText = function (context, text, x, y, color, font) {
+    context.fillStyle = color || '#000000';
+    context.font = font || '16px PT Mono';
+    context.fillText(text, x, y);
+  }
+
+  drawText(ctx, 'Ура вы победили!', CLOUD_X + TEXT_MARGIN_RIGHT, CLOUD_Y + TEXT_MARGIN_TOP);
+  drawText(ctx, 'Список результатов:', CLOUD_X + TEXT_MARGIN_RIGHT, CLOUD_Y + TEXT_MARGIN_TOP + TEXT_LINE_HEIGHT);
 
   var swap = times[0];
   for (var i = 0; i < times.length; i++) {
@@ -53,21 +59,23 @@ window.renderStatistics = function (ctx, names, times) {
     diagram[i] = DIAGRAM_HEIGHT * times[i] / swap;
   }
 
+  var firstColumnX = CLOUD_X + DIAGRAM_MARGIN_RIGHT;
+  var nextColumnX = DIAGRAM_WIDTH + DIAGRAM_SPACE_BETWEEN;
+  var columnsY = CLOUD_Y + DIAGRAM_MARGIN_UP + DIAGRAM_HEIGHT;
+  var timesY = CLOUD_Y + DIAGRAM_MARGIN_UP + DIAGRAM_HEIGHT - DIAGRAM_MARGIN_DOWN / 2;
+  var namesY = CLOUD_Y + CLOUD_HEIGHT - DIAGRAM_MARGIN_DOWN;
+
   for (i = 0; i < COLUMN_NUMBER; i++) {
-    ctx.fillStyle = '#000000';
     var color = '#0000';
     var sub = Math.floor(Math.random() * 256).toString(16);
     color += (sub.length === 1 ? '0' + sub : sub);
+    drawText(ctx, Math.floor(times[i]), firstColumnX + nextColumnX * i, timesY - diagram[i]);
+    drawText(ctx, names[i], firstColumnX + nextColumnX * i, namesY);
     if (names[i] === 'Вы') {
-      ctx.fillText(Math.floor(times[i]), CLOUD_X + DIAGRAM_MARGIN_RIGHT + DIAGRAM_WIDTH * i + DIAGRAM_SPACE_BETWEEN * i, CLOUD_Y + DIAGRAM_MARGIN_UP + DIAGRAM_HEIGHT - diagram[i] - DIAGRAM_MARGIN_DOWN / 2);
-      ctx.fillText(names[i], CLOUD_X + DIAGRAM_MARGIN_RIGHT + DIAGRAM_WIDTH * i + DIAGRAM_SPACE_BETWEEN * i, CLOUD_Y + CLOUD_HEIGHT - DIAGRAM_MARGIN_DOWN);
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(CLOUD_X + DIAGRAM_MARGIN_RIGHT + DIAGRAM_WIDTH * i + DIAGRAM_SPACE_BETWEEN * i, CLOUD_Y + DIAGRAM_MARGIN_UP + DIAGRAM_HEIGHT - diagram[i], DIAGRAM_WIDTH, diagram[i]);
     } else {
-      ctx.fillText(Math.floor(times[i]), CLOUD_X + DIAGRAM_MARGIN_RIGHT + DIAGRAM_WIDTH * i + DIAGRAM_SPACE_BETWEEN * i, CLOUD_Y + DIAGRAM_MARGIN_UP + DIAGRAM_HEIGHT - diagram[i] - DIAGRAM_MARGIN_DOWN / 2);
-      ctx.fillText(names[i], CLOUD_X + DIAGRAM_MARGIN_RIGHT + DIAGRAM_WIDTH * i + DIAGRAM_SPACE_BETWEEN * i, CLOUD_Y + CLOUD_HEIGHT - DIAGRAM_MARGIN_DOWN);
       ctx.fillStyle = color;
-      ctx.fillRect(CLOUD_X + DIAGRAM_MARGIN_RIGHT + DIAGRAM_WIDTH * i + DIAGRAM_SPACE_BETWEEN * i, CLOUD_Y + DIAGRAM_MARGIN_UP + DIAGRAM_HEIGHT - diagram[i], DIAGRAM_WIDTH, diagram[i]);
     }
+    ctx.fillRect(firstColumnX + nextColumnX * i, columnsY - diagram[i], DIAGRAM_WIDTH, diagram[i]);
   }
 }
